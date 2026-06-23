c20f8304ba6e784883623bf3a34209de29052f6f
Ticba <ticba@ticba.me>
2026-06-23 21:59:41 +0800

    day1

---
+++ class1/hello.py
@@ -0,0 +1,10 @@
+print('hellohello')     # 終端機 terminal 輸出文字，文字怎麼打？ -> 兩個引號夾住要顯示的文字 '' 或 ""  ，不能'"
+
+
+# cmd + shift + p 開啟設定功能列表
+# 複製： cmd + c
+# 貼上： cmd + v
+# 整行複製： cmd + c (游標不選取)
+# 整行貼上： cmd + v (游標不選取)
+# 復原上一動： cmd + z
+# 返回下一動： cmd + shift + z
